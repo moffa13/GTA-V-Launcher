dbd143a4f9810042e1294a10fc93a065cd7d1baa
moffa13 <moffajeremy@outlook.com>
2018-11-27 22:33:13 +0100

    removed dir struct in zip file

--- GTA-V-Launcher/copy and sign builds.bat
+++ GTA-V-Launcher/copy and sign builds.bat
@@ -13,7 +13,7 @@ set /p passwrd="Password ? "
 "C:\Program Files (x86)\Windows Kits\10\bin\10.0.16299.0\x64\signtool.exe" ^
 sign /v /f cert\moffa.pfx /p %passwrd% /d "GTA V Launcher" /t http://timestamp.verisign.com/scripts/timstamp.dll "%pth%"
 
-third-party\zip.exe -r "bundle\GTA V Launcher.zip" "bundle\GTA V Launcher.exe"
+third-party\zip.exe -r -j "bundle\GTA V Launcher.zip" "bundle\GTA V Launcher.exe"
 
 ::CALL clean.bat
 
